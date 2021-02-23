3bf177640da29ad87629f6ac26d9e8fb60c91cf9
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-23 00:29:23 -0300

    Traduzido os nomes dos links para português

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Inicial</router-link> |
     <router-link to="/login">Login</router-link> |
-    <router-link to="/logout">Logout</router-link>
+    <router-link to="/logout">Sair</router-link>
   </nav>
 </template>
