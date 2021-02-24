d119c3cf922ab0feec532c4317f3f8969ab2fee5
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-24 02:36:17 -0300

    Realizado importação das bibliotecas do font-awesome e carregamento dos ícones utilizados

--- src/main.js
+++ src/main.js
@@ -4,6 +4,18 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import Meta from 'vue-meta'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome, faSignInAlt, faSignOutAlt, faIdCard, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHome)
+library.add(faSignInAlt)
+library.add(faSignOutAlt)
+library.add(faIdCard)
+library.add(faEdit)
+library.add(faTrash)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
