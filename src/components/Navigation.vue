25b17187d2b8716b14465d0165002826e742dec8
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-25 00:24:27 -0300

    Correção para exibir opções de login conforme usuário estiver logado ou não

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,9 +11,24 @@
     <div id="navbarMain" class="navbar-menu">
       <div class="navbar-end">
         <router-link to="/" class="navbar-item"><font-awesome-icon icon="home" class="mr-1" />Inicial</router-link>
-        <router-link to="/login" class="navbar-item"><font-awesome-icon icon="sign-in-alt" class="mr-1" />Login</router-link>
-        <router-link to="/logout" class="navbar-item"><font-awesome-icon icon="sign-out-alt" class="mr-1" />Sair</router-link>
+        <router-link to="/login" class="navbar-item" v-if="typeof token === 'undefined' || token == null"><font-awesome-icon icon="sign-in-alt" class="mr-1" />Login</router-link>
+        <router-link to="/logout" class="navbar-item" v-if="typeof token !== 'undefined' && token !== null"><font-awesome-icon icon="sign-out-alt" class="mr-1" />Sair</router-link>
       </div>
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      token: undefined
+    }
+  },
+  mounted () {
+    if (typeof localStorage.getItem('token') !== 'undefined') {
+      this.token = localStorage.getItem('token')
+    }
+  }
+}
+</script>
