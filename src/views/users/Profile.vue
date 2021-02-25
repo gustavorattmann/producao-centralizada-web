6bfae0c51a945c33e1c1c761055fbc9961d99e87
Gustavo Rattmann <gustavo_rattmann@hotmail.com.br>
2021-02-25 00:27:43 -0300

    Adicionado link para editar perfil e remover

--- src/views/users/Profile.vue
+++ src/views/users/Profile.vue
@@ -2,11 +2,13 @@
   <div id="profile" class="container">
     <h1 class="title has-text-centered" v-if="typeof status !== 'undefined' && (status == 400 || status == 401 || status == 500)">{{ message }}</h1>
     <section v-else>
-      <h1>Perfil de {{ user.name }}</h1>
+      <h1 class="title has-text-centered">Perfil de {{ user.name }}</h1>
       <p><strong>E-mail:</strong> {{ user.email }}</p>
       <p><strong>Cargo:</strong> {{ user.role }}</p>
       <p v-if="user.situation == 1"><strong>Situação:</strong> Ativo</p>
       <p v-else><strong>Situação:</strong> Inativo</p>
+      <router-link class="has-text-black mr-2" to=""><font-awesome-icon icon="edit" /></router-link>
+      <router-link class="has-text-black" to=""><font-awesome-icon icon="trash" /></router-link>
     </section>
   </div>
 </template>
